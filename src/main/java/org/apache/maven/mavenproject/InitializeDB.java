6006c8611837f4523340ef6ba848750ccbab03c2
albas722 <66467115+albas722@users.noreply.github.com>
2022-04-26 19:16:57 +0200

    Update InitializeDB.java

--- src/main/java/org/apache/maven/mavenproject/InitializeDB.java
+++ src/main/java/org/apache/maven/mavenproject/InitializeDB.java
@@ -14,10 +14,10 @@ public class InitializeDB
 {
     public static void main(String[] args) throws SQLException, IOException {
     	
-    		int[] blobSizes = {100, 1000, 10000};
-	  	int[] dbLengths = {100, 1000, 10000};
-	  	int[] blobTypes = {0, 1};
-	  	Connection conn = null;
+    	int[] blobSizes = {100, 1000, 10000};
+	int[] dbLengths = {100, 1000, 10000};
+	int[] blobTypes = {0, 1};
+	Connection conn = null;
     
 	    // For DB of varying amount of rows
 	    for (int l=0; l<dbLengths.length; l++) {
@@ -26,17 +26,17 @@ public class InitializeDB
 	            // For DB of different blob types
 	            for (int t=0; t<blobTypes.length; t++) {
 	          	  
-		    	  	int blobSize = blobSizes[s];
-		    	  	int dbLength = dbLengths[l];
-		    	  	int type = blobTypes[t];
+		    	    int blobSize = blobSizes[s];
+		    	    int dbLength = dbLengths[l];
+		    	    int type = blobTypes[t];
 		    	  	
-		    	  	String blobType = "Zeros"; 
-		    	  	if (type==1) blobType = "Random"; 
+		    	    String blobType = "Zeros"; 
+		    	    if (type==1) blobType = "Random"; 
 			    	  	
-			      	int runs = 50; // Run 50 Times
+			    int runs = 50; // Run 50 Times
 		            for (int i=0; i<runs; i++) {
 			
-			    	  		String fileName = "Rows-" + dbLength +"_BlobSize-" + blobSize + "_BlobType-" + blobType + "_" + i + ".db";
+			    	String fileName = "Rows-" + dbLength +"_BlobSize-" + blobSize + "_BlobType-" + blobType + "_" + i + ".db";
 
 //			    	        // Create Directory if it does not already exist
 //			    	  		String newDir = ".\\src\\main\\java\\org\\apache\\maven\\mavenproject\\databases\\"+dbLength+"_"+blobSize+"_"+blobType+"\\";
@@ -47,55 +47,55 @@ public class InitializeDB
 			    	         
 			    	        
 //			    	        Path path = Paths.get(newDir+fileName);
-					        Path path = Paths.get(".\\"+fileName);
-					        // Delete file if it already exists.
-					        if (Files.exists(path)){
-					            Files.delete(path); 
-					        }
-					        Path file = Files.createFile(path);
+				Path path = Paths.get(".\\"+fileName);
+				// Delete file if it already exists.
+				if (Files.exists(path)){
+				    Files.delete(path); 
+				}
+				Path file = Files.createFile(path);
 					
 					
-					        Scanner sc;
-					        Connection connection = null;
-					        String txtFile = "src/../DDL.txt";
-					
-					        // Access DDL file
-					        try {
-					            sc = new Scanner(new File(txtFile));
-					        } catch (FileNotFoundException e) {
-					            System.out.println("File not found: " + txtFile.substring(txtFile.lastIndexOf("\\")+1).trim());
-					            return;
-					        } 
-					
-					        
-					        try {
-					            // Establish connection and create a statement
-					            String url = "jdbc:sqlite:" + fileName;
-					            connection = DriverManager.getConnection(url);
-					            Statement statement = connection.createStatement();
-					
-					            // Execute each like of the txtFile as an SQL update
-					            while (sc.hasNextLine()) {
-					                statement.executeUpdate(sc.nextLine()); 
-					            }
-					
-					            // Close Scanner
-					            sc.close();
-					            statement.close();
-					            System.out.println("Database Initialized: " + fileName);
-					
-					        } catch (SQLException e) {
-					            System.out.println(e.getMessage());
-					
-					        } finally {
-					            // Close connection
-					            if (connection != null) {
-					                connection.close(); 
-					            }
-					        }
-		            	}
+				Scanner sc;
+				Connection connection = null;
+				String txtFile = "src/../DDL.txt";
+
+				// Access DDL file
+				try {
+				    sc = new Scanner(new File(txtFile));
+				} catch (FileNotFoundException e) {
+				    System.out.println("File not found: " + txtFile.substring(txtFile.lastIndexOf("\\")+1).trim());
+				    return;
+				} 
+
+
+				try {
+				    // Establish connection and create a statement
+				    String url = "jdbc:sqlite:" + fileName;
+				    connection = DriverManager.getConnection(url);
+				    Statement statement = connection.createStatement();
+
+				    // Execute each like of the txtFile as an SQL update
+				    while (sc.hasNextLine()) {
+					statement.executeUpdate(sc.nextLine()); 
+				    }
+
+				    // Close Scanner
+				    sc.close();
+				    statement.close();
+				    System.out.println("Database Initialized: " + fileName);
+
+				} catch (SQLException e) {
+				    System.out.println(e.getMessage());
+
+				} finally {
+				    // Close connection
+				    if (connection != null) {
+					connection.close(); 
+				    }
+				}
 		            }
-	            }
-	        }
-    	}  
+		     }
+	       }
+	 }
+    }  
 }
